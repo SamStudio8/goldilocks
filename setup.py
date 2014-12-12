6c4b3e69228248084c8bff6b3a5938ab9ec8b962
Sam Nicholls <sam.n@studio8media.co.uk>
2014-12-12 13:52:30 +0000

    0.0.32

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setuptools.setup(
     name="goldilocks",
-    version="0.0.31",
+    version="0.0.32",
     url="https://github.com/samstudio8/goldilocks",
 
     description="Locating genomic regions that are \"just right\".",
