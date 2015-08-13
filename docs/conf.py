3be4a031b4f2e7b6dc3c7e392007e03cb04de37a
Sam Nicholls <sam.n@studio8media.co.uk>
2015-08-13 17:34:11 +0200

    Trick ReadtheDocs in to ignoring missing non std modules

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,11 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
 
+import mock
+MOCK_MODULES = ['numpy', 'matplotlib']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
